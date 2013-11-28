0bf9d592314ea44e29aface2c351630f50973f44
Danilo Bargen <mail@dbrgn.ch>
2013-11-28 12:58:46 +0100

    Implemented voting

--- client/restangular.js
+++ client/restangular.js
@@ -1,1 +1,11 @@
 // Restangular configuration
+
+var app = angular.module('hasglaese');
+
+app.config(function(RestangularProvider) {
+    RestangularProvider.addElementTransformer('entries', false, function(entries) {
+        entries.addRestangularMethod('upvote', 'post', 'up');
+        entries.addRestangularMethod('downvote', 'post', 'down');
+        return entries;
+    });
+});
--- server/rating.js
+++ server/rating.js
@@ -2,20 +2,22 @@ module.exports = function Rating() {
     this.value = 0;
     var voters = [];
     var self = this;
-    
+
     this._up = function(userId) {
-        if (!voters[userId]) {
+        self.value++;
+        /*if (!voters[userId]) {
             self.value++;
             voters[userId] = true;
-        }
+        }*/
         return self.value;
     };
-    
+
     this._down = function (userId) {
-        if (!voters[userId]) {
+        self.value--;
+        /*if (!voters[userId]) {
             self.value--;
             voters[userId] = true;
-        }
+        }*/
         return self.value;
     };
 };
